531f746dfe384f01346af9076ffe2ba8538d032e
alexanderpiers <alex.piers2@gmail.com>
2018-03-23 14:24:03 -0400

    initial port of analysis code to python

---
+++ EM Analysis/plot.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def readComsolFile(filename):
+	"""
+	Reads E&M simulation files into data structures for plotting
+
+	Inputs:
+		filename - path to Comsol data file
+	Outputs:
+
+	"""
+
+
+def plotData(data):
+	"""
+	Plots of 
+	"""
