2dd04f6da0eb16756f8ba05a4e4eb6aa90519e73
alexanderpiers <apiers@uw.edu>
2018-05-04 14:33:21 -0700

    Revert "additional plots for bias voltage analysis. Fixed monte carlo simulation"
    This reverts commit 85b1281e6378805ea4e5c7ae865954853b313b94.

--- EM Analysis/bias_voltage_analysis.py
+++ EM Analysis/bias_voltage_analysis.py
@@ -32,7 +32,7 @@ E = [y, z, data[:,1]/1e6, data[:,2]/1e6] # Setting up the E field input to findi
 
 # Parameters needed for motion
 vDriftHoles = 0.19e2 # um^2/(V*us)
-dt = 0.0005 # us
+dt = 0.001 # us
 yrange = (100, 500) # um
 zrange = (20, 220) # um
 # initialPos = (np.random.uniform(yrange[0], yrange[1]), np.random.uniform(zrange[0], 0.3*zrange[1]))
@@ -58,29 +58,12 @@ ax3.set_xlabel(r'Depth ($\mu m$', fontsize=14)
 ax3.set_ylabel(r'Induced Charge (C)', fontsize=14)
 ax3.set_title(r'Induced Charge Difference. Va=0, Vb=0', fontsize=16)
 
-x = np.linspace(75,375,300)
-zStart = 50
-electrode = []
-for i in range(x.size):
-	xzPath = findMotion((x[i], zStart), E, vDriftHoles, dt, q=1.6e-19)
-	_, _, qdiff = inducedCharge([y, z, weightPhiA], [y, z, weightPhiB], xzPath[:,(0,1)], q=1.6e-19)
-	if qdiff[-1] > 0:
-		electrode.append(1)
-	else:
-		electrode.append(0)
-
-fig6 = plt.figure()
-ax6 = fig6.add_subplot(111)
-ax6.plot(x, np.array(electrode), linewidth=2)
-
-
 # Monte Carlo Simulation. Finding the induced charge for a variety of different initial conditions
-N = 50
+N = 100
 qDiffAll = []
 fig4 = plt.figure()
 ax4 = fig4.add_subplot(111)
 
-# Finding the induced charge for a variety of intial positions
 for i in range(N):
 	initialPos = (np.random.uniform(1.1*yrange[0], 0.9*yrange[1]), np.random.uniform(zrange[0], 0.5*zrange[1]))
 	particleMotion = findMotion(initialPos, E, vDriftHoles, dt, q=1.6e-19)
@@ -89,39 +72,6 @@ for i in range(N):
 	except:
 		print(particleMotion.shape)
 	ax4.plot(particleMotion[:,1], qdiff, 'k', linewidth=0.5)
-
-ax4.set_title('Difference in Induced Charged for N='+str(N)+' Cases', fontsize=16)
-ax4.set_ylabel(r'Induced Charge (C)', fontsize=14)
-ax4.set_xlabel(r'Depth ($\mu m$', fontsize=14)
-
-# Finding which electrode each charged particle
-# Monte carlo with M different intial positions
-M = 100
-fracAtElectrode = []
-biasVoltage =  [0, 40, 80, 120, 160, 200]
-
-for j, volt in enumerate(biasVoltage):
-	countHoles =  0 
-	for i in range(M):
-		initialPos = (np.random.uniform(1.1*yrange[0], 0.9*yrange[1]), np.random.uniform(zrange[0], 0.5*zrange[1]))
-		particleMotion = findMotion(initialPos, [y, z, data[:,1+3*j]/1e6, data[:,2+3*j]/1e6], vDriftHoles, dt, q=1.6e-19)
-		try:
-			qa, qb, qdiff = inducedCharge([y, z, weightPhiA], [y, z, weightPhiB], particleMotion[:,(0,1)], q=1.6e-19)
-		except:
-			print(particleMotion.shape)
-		print(qdiff[-1])
-		if qdiff[-1] > 0:
-			countHoles = countHoles + 1
-
-	# Calculates the fraction of that hit one of the electrodes
-	fracAtElectrode.append(countHoles/M)
-
-fig5 = plt.figure()
-ax5 = fig5.add_subplot(111)
-ax5.plot(np.array(biasVoltage), np.array(fracAtElectrode), linewidth=3)
-ax5.set_title('Fraction of Holes Arriving at Electrode A', fontsize=16)
-ax5.set_ylabel(r'Fraction', fontsize=14)
-ax5.set_xlabel(r'Voltage Bias (V)', fontsize=14)
 
 
 plt.show()
