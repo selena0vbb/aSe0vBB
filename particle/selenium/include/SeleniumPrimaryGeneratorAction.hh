426ff9f3bace39b083a36304e7ad148423824184
alexanderpiers <apiers@uw.edu>
2018-06-21 21:17:10 -0700

    change - switched from general particle source to particleGun for simplicity

--- particle/selenium/include/SeleniumPrimaryGeneratorAction.hh
+++ particle/selenium/include/SeleniumPrimaryGeneratorAction.hh
@@ -6,7 +6,8 @@
 #include "G4VUserPrimaryGeneratorAction.hh"
 #include "globals.hh"
 
-class G4GeneralParticleSource;
+//class G4GeneralParticleSource;
+class G4ParticleGun;
 class G4Event;
 
 class SeleniumPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
@@ -18,10 +19,12 @@ class SeleniumPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 
 		virtual void GeneratePrimaries(G4Event* event);
 
-		const G4GeneralParticleSource* GetParticleGun() const {return fParticleGun;};
+		//const G4GeneralParticleSource* GetParticleGun() const {return fParticleGun;};
+		const G4ParticleGun* GetParticleGun() const { return fParticleGun; };
 
 	private:
-		G4GeneralParticleSource* fParticleGun; // G4 Particle gun pointer
+		//G4GeneralParticleSource* fParticleGun; // G4 Particle gun pointer
+		G4ParticleGun* fParticleGun;
 };
 
 #endif
--- particle/selenium/src/SeleniumPrimaryGeneratorAction.cc
+++ particle/selenium/src/SeleniumPrimaryGeneratorAction.cc
@@ -5,6 +5,7 @@
 #include "G4RunManager.hh"
 #include "G4Event.hh"
 #include "G4GeneralParticleSource.hh"
+#include "G4ParticleGun.hh"
 #include "G4ParticleTable.hh"
 #include "G4ParticleDefinition.hh"
 #include "G4SystemOfUnits.hh"
@@ -18,22 +19,32 @@ SeleniumPrimaryGeneratorAction::SeleniumPrimaryGeneratorAction()
    fParticleGun(nullptr)
 {
 
-	// Create a GPS and assign it to fParticleGun
-	fParticleGun = new G4GeneralParticleSource();
+	// // Create a GPS and assign it to fParticleGun
+	// fParticleGun = new G4GeneralParticleSource();
 
-	// Get the gamma particle from table
+	// // Get the gamma particle from table
 	G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
 	G4String particleType = "gamma";
 	G4ParticleDefinition* particle = particleTable->FindParticle(particleType);
 
-	// Print particle information
-	G4cout << particle << G4endl;
+	// // Print particle information
+	// G4cout << particle << G4endl;
 
-	// Assign particle to source
-	fParticleGun->SetParticleDefinition(particle);
-	// Optional. Setting particle parameters. Can also do with macro
-	// fParticleGun->SetParticleEnergy(122*keV);
+	// // Assign particle to source
+	// fParticleGun->SetParticleDefinition(particle);
+	// // Optional. Setting particle parameters. Can also do with macro
+	// fParticleGun->SetParticleEnergy(122.*keV);
 	// fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,-1.));
+
+	// Using Particle gun for first pass simplicity
+	G4int nOfParticles = 1;
+	fParticleGun = new G4ParticleGun(nOfParticles);
+
+	fParticleGun->SetParticleDefinition(particle);
+	fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., -1.));
+	fParticleGun->SetParticleEnergy(122.*keV);
+
+
 }
 
 SeleniumPrimaryGeneratorAction::~SeleniumPrimaryGeneratorAction()
