777c1b59ee6b973b6332ded471a00e499f6b0c0c
alexanderpiers <apiers@uw.edu>
2018-06-27 15:21:02 -0700

    add - geometric and hit parameters added to header files to be referenced by other parts of the simulation

--- particle/selenium/include/SimulationSettings.hh
+++ particle/selenium/include/SimulationSettings.hh
@@ -3,6 +3,18 @@
 #ifndef SimulationSettings_H
 #define SimulationSettings_H
 
+#include "G4SystemofUnits.hh"
+
 #define OUTPUTDIR "C:\\Users\\alexp\\Documents\\UW\\Research\\Selenium\\aSe0vBB\\particle\\selenium_detector\\ouput\\"
+
+
+// Defining geometries of the problem
+const G4double seleniumXY = 10. * mm;
+const G4double seleniumDepth = 0.2 * mm;
+const G4double goldElectrodeArea = 2 * mm2;
+const G4double goldElectrodeDepth = 10 * nm;
+
+// Sensitive Detector parameters
+const G4String seleniumHitsCollectionName = "SeleniumHitsCollection";
 
 #endif
