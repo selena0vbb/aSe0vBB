891c7510e2fe90ff46134fec933c67caf713becc
alexanderpiers <apiers@uw.edu>
2018-07-31 17:39:28 -0700

    add - analysis of the kapton tiered electrode including weighted potential

--- EM Analysis/polymide_kapton_analysis.py
+++ EM Analysis/polymide_kapton_analysis.py
@@ -84,15 +84,88 @@ def darkCurrentCalculation():
 	# Uses the resistivity of polymide kapton and efield from comsol to get current density
 	# Finds the current by adding the current density through many small areas
 
-	dy = 1 # 1 um dx
-	ydim = 150
-	xdim = 100
+	fileloc =  r'C:\Users\alexp\Documents\UW\Research\Selenium\Coplanar Detector\sim_data\kapton_layer_noHV'
+	filelist = os.listdir(fileloc)
+	# filename = r'C:\Users\alexp\Documents\UW\Research\Selenium\Coplanar Detector\sim_data\kapton_layer_noHV\kapton_layer_analysis_8um_spacing_nohv.txt'
+	kaptonThickness = {'500nm':0.5, "1um":1, "2um":2, "5um":5, "10um":10, "15um":15, "3um":3, "8um":8}
+	resistivity = 1.4e21 # ohm*um. From Dupont data sheet
+	thck = 5.
+	dy = 1. # 1 um dx
+	ydim = 150.
+	xdim = 100.
 	nArea = ydim/dy # number of grids
+	bias = np.arange(100, 301, 20)
+
+	# actual detector parameters
+	detectorLength = 2e3 # um. Corresponds to 2 mm
+	detectorArea = detectorLength**2
+
+	print(filelist)
+
+ 	# Create positions of small area
+	ycent = np.arange(0, ydim, dy) + dy/2 # center position of all the area elements
+	
+
+	current = np.zeros((len(filelist), bias.size))
+	legend = []
+	for findx, file in enumerate(filelist):
+		print(findx)
+		# Read data
+		_, y, z, data = readComsolFileGrid(fileloc + '\\' + file)
+		y, z = y*1e6, z*1e6 # convert to um
+
+		# Remove NaN
+		data = np.nan_to_num(data, copy=False)
+
+		# Split data into phi, Ey, and Ez
+		phi, Ey, Ez = data[:,0:np.amin(data.shape):3], data[:,1:np.amin(data.shape):3], data[:,2:np.amin(data.shape):3]
+
+		# get the kapton thickness from the filename
+		fileIdentifier = file.split('_')[3]
+		thck = kaptonThickness[fileIdentifier]
+		legend.append(fileIdentifier)
+		zcent = np.ones(ycent.size)*thck/2
+
+		for i in range(bias.size):
+			biasCurrent = 0
+			E = [y, z, Ey[:,i]/1e6, Ez[:,i]/1e6]
+			einterp = interpEField2D(ycent, zcent, E)
+			
+			for j in range(ycent.size):
+				biasCurrent += (einterp[j,1]/resistivity*dy)
+
+			current[findx,i] = biasCurrent
+
+	meanCurrentPerUm = current/ydim
+	darkCurrent = meanCurrentPerUm * detectorArea
+	print(darkCurrent)
 
 
+	# Making plots of the dark current related data
+	bmap = brewer2mpl.get_map('Set2', 'Qualitative', 8).mpl_colors
+	plt.rc('font', family='serif')
+	fig, ax = plt.subplots()
+
+	ax.plot(bias, darkCurrent.T*1e12, linewidth=3)
+	ax.set_title('Dark Current Through the Polymide Kapton', fontsize=16)
+	ax.set_xlabel('Bias Voltage (V)', fontsize=16)
+	ax.set_ylabel('Current (pA)', fontsize=16)
+	
+	# necessary bias voltage index
+	biasVoltageIndex = [8, 10, 3, 3, 4, 5, 6]
+
+	for i, line in enumerate(ax.get_lines()):
+		line.set_color(bmap[i])
+		ax.plot(bias[biasVoltageIndex[i]], darkCurrent[i, biasVoltageIndex[i]]*1e12, 'o', markersize=8, markeredgecolor=bmap[i], markeredgewidth=3, markerfacecolor="None")
+	
+	ax.legend(legend)
+	plt.show()
+		
 	return None
 
 
 if __name__ == '__main__':
 	# List of calculations to do. Comment out unnecessary ones to save perfomance
-	effectiveAreaOfDifferentKaptonWidth()
+	
+	# effectiveAreaOfDifferentKaptonWidth()
+	darkCurrentCalculation()
---
+++ EM Analysis/tiered_kapton_weighted_potentials.py
@@ -0,0 +1,87 @@
+# Weighted potential of the tiered design with Polymide kapton spacing
+
+from plot import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+folder = r'C:\Users\alexp\Documents\UW\Research\Selenium\Coplanar Detector\sim_data'
+filelist = ['kapton_layer_analysis_1um_spacing_fullsize.txt',
+			'kapton_layer_analysis_3um_spacing_fullsize.txt',
+			'kapton_layer_analysis_5um_spacing_fullsize.txt',
+			'kapton_layer_analysis_8um_spacing_fullsize.txt']
+
+# filelist = ['kapton_layer_analysis_1um_spacing_fullsize.txt']
+
+figList = []
+axList = [] 
+for findx, file in enumerate(filelist):
+	_, y, z, data = readComsolFileGrid(folder + r'/' + file)
+	y, z = y*1e6, z*1e6
+	phi = data[:,0:np.amin(data.shape):3]
+	wPhiA = phi[:,-2]
+	wPhiB = phi[:,-1] 
+
+	# Regular zoom
+	fig, (ax1, ax2) = plt.subplots(1,2, sharey=True)
+	fig, ax1, cax1 = plotPhi([y,z], wPhiA, 1, figH=(fig, ax1))
+	fig, ax2, cax2 = plotPhi([y,z], wPhiB, 1, figH=(fig, ax2))
+
+	filename = file.split('_')
+	spacingText = filename[3] + ' ' + filename[4]
+
+	fig.suptitle('Weighted Potential for ' + spacingText + ' Kapton', fontsize=16)
+	ax1.set_title(r'Collection Electrode', fontsize=14)
+	ax1.set_xlabel(r'y ($\mu m$)', fontsize=14)
+	ax1.set_ylabel(r'Depth ($\mu m$)', fontsize=14)
+
+	ax2.set_title(r'Control Electrode', fontsize=14)
+	ax2.set_xlabel(r'y ($\mu m$)', fontsize=14)
+
+	fig.colorbar(cax1, ax=ax1)
+	fig.colorbar(cax2, ax=ax2)
+
+	# zoomed in
+	fig2, (ax3, ax4) = plt.subplots(1,2, sharey=True)
+	fig, ax3, cax3 = plotPhi([y,z], wPhiA, 1, figH=(fig2, ax3))
+	fig, ax4, cax4 = plotPhi([y,z], wPhiB, 1, figH=(fig2, ax4))
+
+	filename = file.split('_')
+	spacingText = filename[3] + ' ' + filename[4]
+
+	fig2.suptitle('Weighted Potential for ' + spacingText + ' Kapton', fontsize=16)
+	ax3.set_title(r'Collection Electrode', fontsize=14)
+	ax3.set_xlabel(r'y ($\mu m$)', fontsize=14)
+	ax3.set_ylabel(r'Depth ($\mu m$)', fontsize=14)
+	ax3.set_ylim([np.amin(z), -80])
+	ax3.set_xlim([-125, 125])
+
+	ax4.set_title(r'Control Electrode', fontsize=14)
+	ax4.set_xlabel(r'y ($\mu m$)', fontsize=14)
+	ax4.set_ylim([np.amin(z), -75])
+	ax4.set_xlim([-100, 100])
+	fig2.colorbar(cax3, ax=ax3)
+	fig2.colorbar(cax4, ax=ax4)
+
+	# Plotting a 1D slice of weighted potential
+	wPhiAReshape = np.reshape(wPhiA,(z.size, y.size))
+	wPhiBReshape = np.reshape(wPhiB,(z.size, y.size))
+
+	# Get nearest index to 5 um on the y axis
+	if findx == 0:
+		ySlice = 25
+	else:
+		ySlice = 5
+	yIndx = (np.abs(y-ySlice).argmin())
+	fig3, ax5 = plt.subplots()
+	ax5.plot(-z, wPhiAReshape[:,yIndx], 'k:', -z, wPhiBReshape[:,yIndx], 'k:', -z, wPhiAReshape[:,yIndx]-wPhiBReshape[:,yIndx], 'k', linewidth=3)
+	ax5.set_xlabel(r'Depth ($\mu m$)', fontsize=14)
+	ax5.set_ylabel(r'Weighted Potentail', fontsize=14)
+	ax5.set_title('Y Slice of Weighted Potential and Difference for ' + spacingText, fontsize=16)
+	ax5.set_xlim([-100,100])
+	ax5.set_ylim([0,1])
+
+	figList.extend((fig, fig2, fig3))
+	axList.extend((ax1, ax2, ax3, ax4, ax5))
+
+plt.show()
