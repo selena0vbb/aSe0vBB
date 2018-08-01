485d423d281f82595f8af45de7aeea7dcbfa58a8
alexanderpiers <apiers@uw.edu>
2018-08-01 16:04:38 -0700

    add - scaling the control electrode signal

--- EM Analysis/tiered_kapton_weighted_potentials.py
+++ EM Analysis/tiered_kapton_weighted_potentials.py
@@ -22,6 +22,8 @@ for findx, file in enumerate(filelist):
 	wPhiA = phi[:,-2]
 	wPhiB = phi[:,-1] 
 
+	plt.rc('font', family='serif')
+
 	# Regular zoom
 	fig, (ax1, ax2) = plt.subplots(1,2, sharey=True)
 	fig, ax1, cax1 = plotPhi([y,z], wPhiA, 1, figH=(fig, ax1))
@@ -45,9 +47,6 @@ for findx, file in enumerate(filelist):
 	fig2, (ax3, ax4) = plt.subplots(1,2, sharey=True)
 	fig, ax3, cax3 = plotPhi([y,z], wPhiA, 1, figH=(fig2, ax3))
 	fig, ax4, cax4 = plotPhi([y,z], wPhiB, 1, figH=(fig2, ax4))
-
-	filename = file.split('_')
-	spacingText = filename[3] + ' ' + filename[4]
 
 	fig2.suptitle('Weighted Potential for ' + spacingText + ' Kapton', fontsize=16)
 	ax3.set_title(r'Collection Electrode', fontsize=14)
@@ -74,14 +73,27 @@ for findx, file in enumerate(filelist):
 		ySlice = 5
 	yIndx = (np.abs(y-ySlice).argmin())
 	fig3, ax5 = plt.subplots()
+	phiASlice = wPhiAReshape[:,yIndx]
+	phiBSlice = wPhiBReshape[:,yIndx]
+
+
+	slopeA = (phiASlice[-1]-phiASlice[100])/(z[-1]-z[100])
+	slopeB = (phiBSlice[-1]-phiBSlice[100])/(z[-1]-z[100])
+
+	print(slopeA/slopeB)
+
 	ax5.plot(-z, wPhiAReshape[:,yIndx], 'k:', -z, wPhiBReshape[:,yIndx], 'k:', -z, wPhiAReshape[:,yIndx]-wPhiBReshape[:,yIndx], 'k', linewidth=3)
+	ax5.plot(-z, phiBSlice*slopeA/slopeB, 'r:', -z, phiASlice - phiBSlice*slopeA/slopeB, 'r', linewidth=3)
 	ax5.set_xlabel(r'Depth ($\mu m$)', fontsize=14)
 	ax5.set_ylabel(r'Weighted Potentail', fontsize=14)
 	ax5.set_title('Y Slice of Weighted Potential and Difference for ' + spacingText, fontsize=16)
 	ax5.set_xlim([-100,100])
 	ax5.set_ylim([0,1])
+	lines = ax5.get_lines()
+	ax5.legend((lines[2], lines[4]), ['Not Scaled', 'Scaled'])
+	ax5.text(-75, 0.3, 'Scaling Coefficient = %0.2f' % (slopeA/slopeB))
 
-	figList.extend((fig, fig2, fig3))
-	axList.extend((ax1, ax2, ax3, ax4, ax5))
+	# figList.extend((fig, fig2, fig3))
+	# axList.extend((ax1, ax2, ax3, ax4, ax5))
 
 plt.show()
